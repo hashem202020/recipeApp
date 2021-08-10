14092bd76245b23deb88bf71c31942d22d85727b
apple <apple@Do0da.local>
2021-08-10 12:22:43 +0200

    the first version of Demo recipe app Using MVVM Pattern

---
+++ iosTask/view/cells/filterCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  filterCollectionCell.swift
+//  iosTask
+//
+//  Created by apple on 8/4/21.
+//
+
+import UIKit
+
+class filterCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
